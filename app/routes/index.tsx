a0a02fbc560af0ec77670afcf38ba82c0d9249ef
Yuuki Rika <yvvki@duck.com>
2024-09-14 04:45:17 +0700

    Use JSX renderer

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,15 @@
 export default () => (
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-			<title>Kekkon Nexus</title>
-
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
-			<link
-				href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Noto+Sans+Javanese:wght@400..700&family=Recursive:wght,CASL@300..1000,1&display=swap"
-				rel="stylesheet"
-			/>
-
-			<link rel="stylesheet" href="/style.css" />
-		</head>
-
-		<body>
-			<header>
-				<h1 id="brand" class="writing-vlr">
-					<ruby>
-						結ꦲꦂꦠꦤ꧀ꦠ
-						<rt>
-							けっこん
-						</rt>
-					</ruby>
-				</h1>
-				<p>
-					Kekkon<br />Nexus
-				</p>
-			</header>
-		</body>
-	</html>
+	<header>
+		<h1 id="brand" class="writing-vlr">
+			<ruby>
+				結ꦲꦂꦠꦤ꧀ꦠ
+				<rt>けっこん</rt>
+			</ruby>
+		</h1>
+		<p>
+			Kekkon
+			<br />
+			Nexus
+		</p>
+	</header>
 );

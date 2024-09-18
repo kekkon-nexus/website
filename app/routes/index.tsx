e8a608a00d99f6b8162346f4ec810ccb7c7c9bad
Yuuki Rika <yvvki@duck.com>
2024-09-18 08:24:59 +0700

    Replace tag for main

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,17 @@
 export default () => (
-	<header>
-		<h1 id="brand">
-			<ruby>
-				結ꦲꦂꦠꦤ꧀ꦠ
-				<rt>けっこん</rt>
-			</ruby>
-		</h1>
-		<p id="title">
-			Kekkon
-			<br />
-			Nexus
-		</p>
-	</header>
+	<>
+		<main>
+			<h1 id="brand">
+				<ruby>
+					結ꦲꦂꦠꦤ꧀ꦠ
+					<rt>けっこん</rt>
+				</ruby>
+			</h1>
+			<p id="title">
+				Kekkon
+				<br />
+				Nexus
+			</p>
+		</main>
+	</>
 );
